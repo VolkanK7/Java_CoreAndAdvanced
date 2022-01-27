d11317634a610fbfa0853af5bd4ec876ee9e87b6
VolkanK7 <volkan.kaya007@gmail.com>
2022-01-27 19:51:10 +0300

    Stream Operations

---
+++ Stream/Stream2/src/User.java
@@ -0,0 +1,67 @@
+public class User {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String nationality;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public User(long id, String firstName, String lastName, int age, String nationality) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.nationality = nationality;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", nationality='" + nationality + '\'' +
+                '}';
+    }
+}
+
